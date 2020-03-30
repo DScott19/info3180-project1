40a34283b1b8718510da408995f2b9f23805aaa3
Deandra Scott <scott_deandra@yahoo.com>
2020-03-30 17:56:13 -0500

    Project 1 code

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password123@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://hhvyppyqidlyot:db72459834ff6efd52e9418ce825fa59942aa57329ced9a6ab6ab324cb0ce4a8@ec2-18-235-20-228.compute-1.amazonaws.com:5432/dd5sg5odja5546"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 app.config['UPLOAD_FOLDER']= './app/static/uploads'
 
